80a8df56b057bc4168620f05a68bbf9672c7b46d
Jwiggiff <friedman.josh03@gmail.com>
2023-05-08 21:37:56 -0400

    Add contact animation

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,22 +1,30 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./Contact.module.scss";
+import { useState } from "react";
 
 export default function Contact() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.contact}>
-      <h5 className={styles.header}>Contact</h5>
+    <div className={styles.contact + " " + (isOpen ? styles.open : "")}>
+      <button
+        onClick={() => setIsOpen((open) => !open)}
+        className={styles.button}
+      >
+        Contact
+      </button>
+
       <ul className={styles.contactlist}>
-        <li>
+        <li className={styles.contactlink}>
           <Link href="#">A</Link>
         </li>
-        <li>
-          <Link href="#">C</Link>
-        </li>
-        <li>
-          <Link href="#">D</Link>
-        </li>
-        <li>
+        <li className={styles.contactlink}>
           <Link href="#">B</Link>
+        </li>
+        <li className={styles.contactlink}>
+          <Link href="#">C</Link>
         </li>
       </ul>
     </div>
